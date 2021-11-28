f9ac56cce86d474c4ad7a1ef64d49e6d1d84fbf3
galjotmaitena <galjotmaitena@gmail.com>
2021-11-28 18:05:05 -0300

    commit

---
+++ tp3_windows/Controller.h
@@ -0,0 +1,88 @@
+//controlar la lectura del archivo, PATH direccion del archivo, carpeta en la que esta el archivo
+
+/** \brief Carga los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_loadFromText(char* path , LinkedList* pArrayListEmployee);
+/** \brief Carga los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_loadFromBinary(char* path , LinkedList* pArrayListEmployee);
+/** \brief Alta de empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_addEmployee(LinkedList* pArrayListEmployee);
+/** \brief Modificar datos de empleado
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_editEmployee(LinkedList* pArrayListEmployee);
+/** \brief Baja de empleado
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_removeEmployee(LinkedList* pArrayListEmployee);
+/** \brief Listar empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_ListEmployee(LinkedList* pArrayListEmployee);
+/** \brief Ordenar empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_sortEmployee(LinkedList* pArrayListEmployee);
+/** \brief Guarda los datos de los empleados en el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_saveAsText(char* path , LinkedList* pArrayListEmployee);
+/** \brief Guarda los datos de los empleados en el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_saveAsBinary(char* path , LinkedList* pArrayListEmployee);
+
+/// \fn int controller_getLastId(char*, int*)
+/// \brief Lee un archivo de texto que contiene el ultimo id, y a este se le suma 1 para sacar el proximo id
+/// \param path
+/// \param id Posicion de memoria donde se guardara el id
+/// \return retona 0 si se obtubo el id, o -1 si no se logro conseguirlo
+int controller_getLastId(char* path, int* id);
+
+/// \fn int controller_saveLastId(char*, int)
+/// \brief Guarda el nuevo id si se pudo añadir exitosamente al empleado
+/// \param path
+/// \param id Id a guardar
+/// \return retona 0 si se guardo el id, o -1 si no se logro guardarlo
+int controller_saveLastId(char* path, int id);
